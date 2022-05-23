1a392e9fb024c06b5b5f322ff4af4f478f567844
ilhamsurya <ilhamsurya.m@gmail.com>
2022-05-23 16:49:41 +0700

    update relation user for comment & subcomment

--- models/user.js
+++ models/user.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.hasMany(models.Post);
+      User.hasMany(models.Comment);
+      User.hasMany(models.SubComment);
     }
   }
   User.init(
