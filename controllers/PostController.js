5c5cbd699c28b38328ca23e08ec09b33f92904b7
ilhamsurya <ilhamsurya.m@gmail.com>
2022-05-23 11:28:22 +0700

    add method to create post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -19,6 +19,25 @@ class PostController {
       ctx.response.body = errors;
     }
   }
+  static async create(ctx) {
+    const UserId = ctx.user.id;
+    if (ctx.request.body.title === undefined) {
+      ctx.request.body.title = null;
+    }
+    if (ctx.request.body.content === undefined) {
+      ctx.request.body.content = null;
+    }
+    const { title, content } = ctx.request.body;
+    try {
+      const newPost = await Post.create({ title, content, UserId });
+      ctx.response.status = 201;
+      ctx.response.body = newPost;
+    } catch (err) {
+      const { status, errors } = errorHandler(err);
+      ctx.response.status = status;
+      ctx.response.body = errors;
+    }
+  }
 }
 
 module.exports = PostController;
