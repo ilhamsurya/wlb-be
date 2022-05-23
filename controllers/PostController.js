d03007a9e70701f9e994227e375abbbae3577ffa
ilhamsurya <ilhamsurya.m@gmail.com>
2022-05-23 13:10:09 +0700

    add update and delete method in post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -41,6 +41,50 @@ class PostController {
       ctx.response.body = errors;
     }
   }
+  static async update(ctx) {
+    const id = ctx.request.query.id;
+    if (ctx.request.body.title === undefined) {
+      ctx.request.body.title = null;
+    }
+    if (ctx.request.body.content === undefined) {
+      ctx.request.body.content = null;
+    }
+    const { title, content } = ctx.request.body;
+    try {
+      const updatedPost = await Post.update(
+        { title, content },
+        {
+          where: { id },
+          returning: true,
+        },
+      );
+      ctx.response.status = 200;
+      ctx.response.body = {
+        message: 'Successfully Updated',
+        updatedPost,
+      };
+    } catch (err) {
+      const { status, errors } = errorHandler(err);
+      ctx.response.status = status;
+      ctx.response.body = errors;
+    }
+  }
+  static async delete(ctx) {
+    const id = ctx.request.query.id;
+    try {
+      const deletedPost = await Post.findByPk(id);
+      await Post.destroy({ where: { id } });
+      ctx.response.status = 200;
+      ctx.response.body = {
+        message: 'Successfully Deleted',
+        deletedPost,
+      };
+    } catch (err) {
+      const { status, errors } = errorHandler(err);
+      ctx.response.status = status;
+      ctx.response.body = errors;
+    }
+  }
 }
 
 module.exports = PostController;
