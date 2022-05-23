a1c10f8f95b011eb073d4ac8c70016a0725aa2d6
ilhamsurya <ilhamsurya.m@gmail.com>
2022-05-23 13:11:05 +0700

    add new routing for update & delete post

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -1,7 +1,11 @@
 const PostController = require('../../controllers/PostController');
-
+const { authentication } = require('../../middleware/auth');
 function postsRoute(router) {
-  router.get('/posts', PostController.getAll);
+  router
+    .post('/posts', authentication, PostController.create)
+    .get('/posts', PostController.getAll)
+    .put('/posts', authentication, PostController.update)
+    .delete('/posts', authentication, PostController.delete);
 }
 
 module.exports = postsRoute;
