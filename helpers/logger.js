523e11ecd7cc9072f229450fac7e5225b9704e15
ilhamsurya <ilhamsurya.m@gmail.com>
2022-05-26 15:12:17 +0700

    add helper to create log

---
+++ helpers/logger.js
@@ -0,0 +1,61 @@
+const { User } = require('../models');
+const { verifyToken } = require('./jwtAuth');
+const axios = require('axios');
+const env = process.env.NODE_ENV || 'development';
+
+async function log(
+  path,
+  userObject,
+  accessToken,
+  reqStartTime,
+  reqObj,
+  resObj,
+) {
+  const accessTime = Date.now() - reqStartTime;
+  let userDetail;
+
+  if (accessToken === 'null' || accessToken === '') {
+    accessToken = null;
+  }
+  if (userObject) {
+    const user = await User.findOne({ where: userObject });
+
+    userDetail = user;
+  } else if (accessToken) {
+    const userData = verifyToken(accessToken);
+    const { id, username, email, status } = userData;
+    const user = await User.findOne({ where: { id, username, email, status } });
+    userDetail = user;
+  } else {
+    userDetail = {
+      id: null,
+      username: null,
+      email: null,
+      password: null,
+      status: null,
+      createdAt: null,
+      updatedAt: null,
+    };
+  }
+
+  try {
+    let logsUrl;
+    if (env === 'production') {
+      logsUrl = process.env.LOGS_URL;
+    } else {
+      logsUrl = 'http://localhost:3003/logs';
+    }
+    await axios.post(logsUrl, {
+      path,
+      userDetail,
+      accessTime,
+      reqObj,
+      resObj,
+    });
+    console.log('logged successfully');
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = log;
